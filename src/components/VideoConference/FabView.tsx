23d026f135891b9c5f2c3f0e701b35ae3f5afdb1
Ilya Katz <ilyakatz@gmail.com>
2020-05-29 15:09:06 -0700

    Add floating button on top of the confernce

--- src/components/VideoConference/FabView.tsx
+++ src/components/VideoConference/FabView.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import clsx from "clsx";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Zoom from "@material-ui/core/Zoom";
 import Fab from "@material-ui/core/Fab";
-import AddIcon from "@material-ui/icons/Add";
-import EditIcon from "@material-ui/icons/Edit";
-import UpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { green } from "@material-ui/core/colors";
 import Box from "@material-ui/core/Box";
+import CustomizedDialogs from "./CustomizedDialogs";
 
 function TabPanel(props: any) {
   const { children, value, index, ...other } = props;
@@ -72,7 +66,7 @@ export default function FloatingActionButtonZoom() {
     <div className={classes.root}>
       <Zoom key="primary" in={true} timeout={transitionDuration} unmountOnExit>
         <Fab aria-label="Add" className={classes.fab} color="primary">
-          <EditIcon />
+          <CustomizedDialogs />
         </Fab>
       </Zoom>
     </div>
