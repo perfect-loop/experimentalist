f51a2a9d736851af42292f0e4133dcafa8354eda
Ilya Katz <ilyakatz@gmail.com>
2020-06-06 09:12:56 -0700

    Organize drawer

---
+++ client/src/components/PersistentDrawerLeft/UnAuthenticatedIcons.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { API_DOMAIN } from '../../util/config';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import { useAuth0 } from '../../util/react-auth0-spa';
+
+export default function UnAuthenticatedIcons(props: any) {
+  const auth0 = useAuth0();
+  const isAuthenticated = auth0.isAuthenticated;
+  if (isAuthenticated) {
+    return <></>
+  } 
+  else {
+    return (
+      <>
+        <a href={`${API_DOMAIN}/api/auth/login`}>
+          <ListItem button key={'Login'}>
+            <ListItemIcon><VpnKeyIcon /></ListItemIcon>
+            <ListItemText primary={'Login'} />
+          </ListItem>
+        </a>
+      </>
+    );
+  }
+}
