3fc0059467c4e0aebf37381e2bda439e5c5803d1
Ilya Katz <ilyakatz@gmail.com>
2020-06-10 17:46:45 -0700

    Show error if unable to upload participants

--- client/src/components/Events/Participants/index.tsx
+++ client/src/components/Events/Participants/index.tsx
@@ -5,6 +5,8 @@ import EventStore from "../storage/EventStore";
 import AllParticipants from "./AllParticipants";
 import ParticipantsStore, { IRawUploadedData } from "../storage/ParticipantsStore";
 import { CSVReader } from "react-papaparse";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle/AlertTitle";
 
 interface IState {
   participantsStore: ParticipantsStore;
@@ -38,8 +40,18 @@ export default class Index extends Component<IProps, IState> {
   public render() {
     return (
       <>
+        {this.state.participantsStore.state === "error" &&
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            Unable to upload participants. Please make sure there are no duplicate entries
+        </Alert>
+        }
         <AllParticipants participantsStore={this.state.participantsStore} eventStore={this.state.eventStore} />
-        <CSVReader onDrop={this.handleOnDrop} style={{}} config={{}}>
+        <CSVReader onDrop={this.handleOnDrop} style={{}} config={{
+          header: false, error: (e: any) => {
+            console.error("Unable to open file", e);
+          }
+        }} >
           <span>Drop CSV file here or click to upload.</span>
         </CSVReader>
       </>
