8e06b106ff376261cf050d8cb7e7d4f4b54046b9
Ilya Katz <ilyakatz@gmail.com>
2020-06-15 17:24:49 -0700

    Correct header

--- client/src/components/Events/Index/HostHeader.tsx
+++ client/src/components/Events/Index/HostHeader.tsx
@@ -4,7 +4,7 @@ import { StyledTableCell } from "./StyledTableCell";
 export default function HostHeader() {
   return (
     <>
-      <StyledTableCell align="center"> Participants </StyledTableCell>
+      <StyledTableCell align="center"> Title </StyledTableCell>
       <StyledTableCell align="center"> Settings </StyledTableCell>
       <StyledTableCell align="center"> Go </StyledTableCell>
       <StyledTableCell align="center"> Participants </StyledTableCell>
