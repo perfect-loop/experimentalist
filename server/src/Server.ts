353de68eb69c8f21cfb861d244d504f5a4ce226a
Ilya Katz <ilyakatz@gmail.com>
2020-06-20 19:06:25 -0700

    Linting

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -73,14 +73,14 @@ app.use(express.urlencoded({ extended: false }));
  *                              HTTPS
  ***********************************************************************************/
 if (process.env.NODE_ENV != "development") {
-  app.use(function (req, res, next) {
+  app.use(function(req, res, next) {
     if (req.secure) {
       // request was via https, so do no special handling
       next();
     } else {
-      console.log("Redirecting to https")
+      console.log("Redirecting to https");
       // request was via http, so redirect to https
-      res.redirect('https://' + req.headers.host + req.url);
+      res.redirect("https://" + req.headers.host + req.url);
     }
   });
 }
