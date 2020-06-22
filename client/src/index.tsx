f263648ddc14022562521aea0cf24ae210022629
Ilya Katz <ilyakatz@gmail.com>
2020-06-21 18:54:14 -0700

    Fix sentry

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { Auth0Provider } from "./util/react-auth0-spa";
 import history from "./util/history";
 import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from "./util/config";
 import App from "./App";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
 // A function that routes the user to the right place
 // after login
@@ -17,7 +17,7 @@ const onRedirectCallback = (appState: any) => {
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_URL,
-  environment: process.env.NODE_ENV
+  environment: process.env.NODE_ENV,
 });
 
 ReactDOM.render(
