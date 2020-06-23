ce513fbaf810a1097f1af29dfec734cf451fe920
anson <ansonchongwus44@gmail.com>
2020-06-23 15:16:14 -0700

    add comment

--- client/src/components/Forms/SelectAdapter.tsx
+++ client/src/components/Forms/SelectAdapter.tsx
@@ -1,36 +1,35 @@
-// Select element, to be implemment with react-final-form
 
+import React from "react";
+import { SyntheticEvent } from "react";
+import { Select, InputLabel, MenuItem } from "@material-ui/core";
 
-// import React from "react";
-// import { SyntheticEvent } from "react";
-// import { Select, InputLabel, MenuItem } from "@material-ui/core";
+// <Select> element, to be implemmented with react-final-form
+const SelectAdapter = ({ input, meta, label, options, ...rest }: { input: any; label: any; options: any; meta: any }) => {
+  return (
+    <>
+      <InputLabel id={label + "-label"}>{label}</InputLabel>
+      <Select
+        {...input}
+        {...rest}
+        labelId={label + "-label"}
+        id={label}
+        onChange={(event: SyntheticEvent) => {
+          const target = event.target as HTMLSelectElement;
+          const value = target.value;
+          console.log(`value is ${value}`);
+          input.onChange(value);
+        }}
+        errorText={meta.touched ? meta.error : ""}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        {options.map((option: any) => (
+          <MenuItem value={option}>{option}</MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+};
 
-// const SelectAdapter = ({ input, meta, label, options, ...rest }: { input: any; label: any; options: any; meta: any }) => {
-//   return (
-//     <>
-//       <InputLabel id={label + "-label"}>{label}</InputLabel>
-//       <Select
-//         {...input}
-//         {...rest}
-//         labelId={label + "-label"}
-//         id={label}
-//         onChange={(event: SyntheticEvent) => {
-//           const target = event.target as HTMLSelectElement;
-//           const value = target.value;
-//           console.log(`value is ${value}`);
-//           input.onChange(value);
-//         }}
-//         errorText={meta.touched ? meta.error : ""}
-//       >
-//         <MenuItem value="">
-//           <em>None</em>
-//         </MenuItem>
-//         {options.map((option: any) => (
-//           <MenuItem value={option}>{option}</MenuItem>
-//         ))}
-//       </Select>
-//     </>
-//   );
-// };
-
-// export default SelectAdapter;
+export default SelectAdapter;
