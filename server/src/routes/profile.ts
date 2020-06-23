07c7af26b93a1c81c68f97599b4c2b1cd6b778f8
anson <ansonchongwus44@gmail.com>
2020-06-22 20:52:08 -0700

    create profile form

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,14 +1,38 @@
 import express from "express";
 import secured from "../lib/middleware/secured";
-import {}
+import { IProfile, Profile } from "api/Profile"
+import { Auth0User } from "types/auth0";
+
 const router = express.Router();
 
-router.get("/profile", (req, res, next) => {
+router.get("/profile", secured(), async (req: any, res, next) => {
+  const email = req.user.email;
+  console.log("requesting profile")
+  // const profile = await Profile.find({email})
+  // console.log(profile); 
   res.status(200).send("Pong");
 });
 
-router.post("/profile", secured(), async (req: any, res, next) => {
-  const id = req.parms.id;
+router.post("/profile", async (req: any, res, next) => {
+  // const id = req.parms.id;
+  console.log("got request to create profile");
+  const body = req.body;
+
+  console.log(`profilAttr is ${JSON.stringify(body)}`);
+
+  const profile = new Profile(body) as IProfile;
+
+  // profile
+  //   .save()
+  //   .then((newProfile: IProfile) => {
+  //     console.log(`profile is ${newProfile}`);
+  //     res.json(newProfile);
+  //   })
+  //   .catch((reason: any) => {
+  //     console.log(reason.constructor);
+  //     res.status(500).send(reason.message);
+  //   });
+  console.log(profile);
 });
 
 export default router;
