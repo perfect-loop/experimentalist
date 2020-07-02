8cd9573ed8d89360b3e55834cbf8b8125d6a14dd
anson <ansonchongwus44@gmail.com>
2020-07-02 15:06:08 -0700

    create compensation controllers

---
+++ server/src/controllers/CompensationsControllers.ts
@@ -0,0 +1,103 @@
+import { Request, Response, NextFunction } from "express";
+import { IParticipation, Participation } from "api/Participations";
+import { Profile, IProfile } from "api/Profiles";
+import { User } from "api/Users";
+import { ICompensation, Compensation } from "api/Compensations";
+import { IEvent, Event } from "api/Events";
+import { Auth0User } from "types/auth0";
+
+export default class CompensationsController {
+  public async userCompensation(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const user: Auth0User | undefined = req.user;
+    if (user === undefined) return;
+
+    const event = await this.getEvent(req.params.id);
+    if (event === null) {
+      res.status(404).send("Event not found");
+    }
+
+    const participation: IParticipation | null = await Participation.findOne({
+      $and: [{ email: user.email }, { "event._id": event._id }]
+    });
+
+    if (participation === null) {
+      res.status(404).send("Participation not found!");
+      return;
+    }
+    // for normal user, fetch all compensation he can receive
+    const receiverId: any = participation?._id;
+    const compensation: ICompensation | null = await Compensation.findOne({
+      receiverId
+    });
+
+    const profile = await Profile.findOne({ userId: user._id });
+
+    res.status(200).json([{ profile, compensation }]);
+  }
+
+  public async adminCompensation(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const user: Auth0User | undefined = req.user;
+    if (user === undefined) return;
+
+    const event = await this.getEvent(req.params.id);
+    if (event === null) {
+      res.status(404).send("Event not found");
+    }
+
+    const adminParticipation: IParticipation | null = await Participation.findOne(
+      { $and: [{ "event._id": event._id }, { email: user.email }] }
+    );
+
+    if (adminParticipation === null) {
+      res.status(404).send("Participation not found!");
+      return;
+    }
+
+    const adminId: any = adminParticipation?._id;
+    // all compensations the admin needs to send
+    const adminCompensation: ICompensation[] = await Compensation.find({
+      senderId: adminId
+    }).populate("receiverId");
+
+    // base on the compensation, get all receiverIds, fetch their email from
+    // participation, then fetch their email and corresponding id
+    // map email to corresponding total compensation
+    const emailMap: {[identifier: string]: any} = {};
+    const emails = adminCompensation.map((c: any) => c.receiverId.email);
+
+    adminCompensation.forEach((compensation: any) => {
+      const email: string = compensation.receiverId.email;
+      emailMap[email] = {};
+      compensation.receiverId = compensation.receiverId._id;
+      emailMap[email].compensation = compensation;
+    });
+
+    const userId = (await User.find({ email: { $in: emails } })).map(
+      u => u._id
+    );
+    const profiles = await Profile.find({ userId: { $in: userId } }).populate(
+      "userId"
+    );
+    profiles.forEach((p: any) => {
+      const email = p.userId.email;
+      p.userId = p.userId._id;
+      emailMap[email].profile = p;
+    });
+
+    const result = Object.values(emailMap);
+    res.status(200).json(result);
+  }
+
+  private async getEvent(id: string) {
+    const event = (await Event.findById(id)) as IEvent;
+    return event;
+  }
+}
--- server/src/routes/compensation.ts
+++ server/src/routes/compensation.ts
@@ -1,82 +1,14 @@
 import express from "express";
 import secured from "../lib/middleware/secured";
-import { ICompensation, Compensation } from "api/Compensations";
-import { IParticipation, Participation } from "api/Participations";
-import { IUserSchema, User } from "api/Users";
-import { Auth0User } from "types/auth0";
-import { Schema } from "mongoose";
-import { Profile, IProfile } from "api/Profiles";
-
+import CompensationsController from "../controllers/CompensationsControllers";
 const router = express.Router();
 
-interface IUserCompensation {
-  profile: IProfile,
-  compensation: number,
-}
+router.get("/compensations/:id.json", secured(), async (req: any, res, next) => {
+  new CompensationsController().userCompensation(req, res, next)
+});
 
-router.get("/compensation.json", secured(), async (req: any, res, next) => {
-  // return a hashMap, email maps to profile & compensation
-  const email = req.user.email;
-  const participation: IParticipation[] = await Participation.find({
-    email
-  });
-
-  if (participation.length === 0) {
-    res.status(404).send("Event not found!");
-    return;
-  }
-  // for normal user, fetch all compensation he can receive
-  const participationId: any[] = participation.map(p => p._id);
-  const compensation: ICompensation[] = await Compensation.find({
-    receiverId: { $in: participationId }
-  });
-  // sum compensation
-  const totalCompensation = compensation.reduce( (acc, c) => {
-    return acc + c.amount
-  }, 0)
-
-  const normalUser = await User.findOne({ email });
-  if (normalUser === null) {
-    res.status(404).send("Profile not found")
-    return
-  }
-  const profile = (await Profile.find({ userId: normalUser._id}))[0]
-  const userObj: { [Identifier: string]: IUserCompensation } = {};
-  userObj[email] = {
-    profile,
-    compensation: totalCompensation
-  }
-  res.status(200).json(userObj);
-
-  // for admin user, find all his participation
-  const adminId: any[] = participation.map(p => p._id);
-  // compensation the admin needs to send
-  const adminCompensation: ICompensation[] = await Compensation.find({
-    senderId: { $in: adminId }
-  }).populate("receiverId");
-  // base on the compensation, get all receiverIds, fetch their email from
-  // participation, then fetch their email and corresponding id
-  // map email to corresponding total compensation
-  const emailMap: any = {};
-
-  adminCompensation.forEach((compensation: any) => {
-    const email: string = compensation.receiverId.email;
-    if (!(email in emailMap)) emailMap[email] = { compensation: 0 };
-    emailMap[email].compensation += compensation.amount;
-  });
-
-  const emails = Object.keys(emailMap);
-  const userId = (await User.find({ email: { $in: emails } })).map(u => u._id);
-  const profiles = await Profile.find({ userId: { $in: userId } }).populate(
-    "userId"
-  );
-  profiles.forEach((p: any) => {
-    const email = p.userId.email;
-    p.userId = p.userId._id;
-    emailMap[email].profile = p;
-  });
-
-  res.status(200).json(emailMap);
+router.get("/my/compensations/:id.json", secured(), async (req: any, res, next) => {
+  new CompensationsController().adminCompensation(req, res, next);
 });
 
 router.post("/compensation.json", secured(), async (req: any, res, next) => {
