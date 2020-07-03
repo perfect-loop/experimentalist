aa4353a3f1ea8845a3aa56d2fc3e479e8142b80c
anson <ansonchongwus44@gmail.com>
2020-07-03 03:29:07 -0700

    fix rebase error

---
+++ api/lib/Participations.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Participation = exports.ParticipationsSchema = void 0;
+var mongoose = require("mongoose");
+exports.ParticipationsSchema = new mongoose.Schema({
+    email: {
+        type: String,
+    },
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "events"
+    },
+    role: {
+        type: String,
+        enum: ["attendee", "host"],
+        default: "attendee"
+    },
+    anonymousName: {
+        type: String
+    },
+    instructions: {
+        type: String
+    },
+    attendedAt: {
+        type: Date
+    }
+}, {
+    timestamps: true
+});
+exports.ParticipationsSchema.index({ "email": 1, "event": 1, role: 1 }, { unique: true });
+exports.Participation = mongoose.model("participation", exports.ParticipationsSchema);
