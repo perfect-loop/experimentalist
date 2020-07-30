435bb6344eef53cc3115fd07a3755eeae63e5d96
Ilya Katz <ilyakatz@gmail.com>
2020-07-30 09:25:43 -0700

    Mobile not supported

---
+++ client/src/Mobile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  mobileImg: {
+    width: "100%",
+  },
+});
+export function Mobile() {
+  const classes = useStyles();
+  return (
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid item xs={9}>
+        <img className={classes.mobileImg} src="/images/mobile.jpeg" alt="Logo" />
+        <Typography variant="h3">Device not supported</Typography>
+        <Typography variant="h5">Please visit the website on your desktop or laptop computer.</Typography>
+      </Grid>
+    </Grid>
+  );
+}
