69d59bb0bbb8c4450a9d17e3aa88bf742beb51de
Deborah Enoma <enomaomorogieva@gmail.com>
2020-08-10 16:28:19 -0400

    Add check for mobile first

--- client/src/BrowserViewWrapper.tsx
+++ client/src/BrowserViewWrapper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { isChrome, isFirefox } from "react-device-detect";
+import { isChrome, isFirefox, isMobile } from "react-device-detect";
 import { Auth0Provider } from "./util/react-auth0-spa";
 import history from "./util/history";
 import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from "./util/config";
@@ -13,20 +13,27 @@ const onRedirectCallback = (appState: any) => {
   history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
 };
 
-const BrowserViewWrapper = () =>
-  isChrome || isFirefox ? (
-    <Auth0Provider
-      domain={AUTH0_DOMAIN}
-      client_id={AUTH0_CLIENT_ID}
-      redirect_uri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <FlagsProvider features={{ venmoLogin: true }}>
-        <App />
-      </FlagsProvider>
-    </Auth0Provider>
-  ) : (
-    <UnsupportedBrowser />
-  );
+const BrowserViewWrapper = () => {
+  if (isMobile) {
+    return <div />;
+  }
+
+  if (isChrome || isFirefox) {
+    return (
+      <Auth0Provider
+        domain={AUTH0_DOMAIN}
+        client_id={AUTH0_CLIENT_ID}
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+      >
+        <FlagsProvider features={{ venmoLogin: true }}>
+          <App />
+        </FlagsProvider>
+      </Auth0Provider>
+    );
+  }
+
+  return <UnsupportedBrowser />;
+};
 
 export default BrowserViewWrapper;
