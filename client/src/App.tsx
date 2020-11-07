5460469a04ca99904976943dc77135d1aeba18dc
Ilya Katz <ilyakatz@gmail.com>
2020-11-07 09:03:16 -0800

    Unstable internet connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,9 @@ import { EventRoutes } from "./components/Routes/EventRoutes";
 import "./App.css";
 import FullStory from "react-fullstory";
 import { FULLSTORY_CODE } from "./util/config";
+import { Offline } from "react-detect-offline";
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
 const App: React.FC = () => {
   const { isInitializing } = useAuth0();
@@ -17,6 +20,11 @@ const App: React.FC = () => {
     <Router>
       <div className="App">
         {FULLSTORY_CODE && <FullStory org={FULLSTORY_CODE} />}
+        <Offline>
+          <Snackbar anchorOrigin={{ vertical: "top", horizontal: "center" }} open={true} autoHideDuration={6000}>
+            <Alert severity="error">Your internet connection is unstable</Alert>
+          </Snackbar>
+        </Offline>
         <Switch>
           <EventRoutes />
         </Switch>
