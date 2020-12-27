cf6f5c7f75a2ab5546860b2d7ea9b69d949f124e
Ilya Katz <ilyakatz@gmail.com>
2020-12-26 20:39:59 -0800

    Update admit all functionality

--- client/src/components/VideoConference/speeddial/AdmitAll/index.tsx
+++ client/src/components/VideoConference/speeddial/AdmitAll/index.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import GroupAddIcon from "@material-ui/icons/GroupAdd";
-import $ from "jquery";
+import { ZoomMtg } from "@zoomus/websdk";
+import { IAttendee, IZoomResult } from "../../types";
 
 const handleClick = () => {
-  if ($("#wc-container-left.show-participants").length === 0) {
-    alert("Please open participants tab before admiting participants");
+  ZoomMtg.getAttendeeslist({
+    success: function(res: IZoomResult) {
+      console.log("get getAttendeeslist");
+      const participants = res.result!!.attendeesList.filter(p => p.isHold);
+      admitNextParticipant(participants);
+    },
+  });
+};
+
+const admitNextParticipant = async (attendees: IAttendee[]) => {
+  var user = attendees.pop();
+  if (user === undefined) {
+    return;
   }
-  $(".waitingroom-ul")
-    .find(".btn:contains('Admit')")
-    .trigger("click");
+  ZoomMtg.putOnHold({
+    userId: user.userId,
+    hold: false, // take user out of waiting room
+    success: async function(res: any) {
+      console.log(`Success is `, res);
+      await new Promise(r => setTimeout(r, 5000));
+      admitNextParticipant(attendees);
+    },
+    error: async function(res: any) {
+      console.log(`Error is `, res);
+      await new Promise(r => setTimeout(r, 5000));
+      admitNextParticipant(attendees);
+    },
+  });
 };
 
 export default function AdmitAll() {
